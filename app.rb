7f3dc34d6731269e518c68e56d0cd3d737c62dc8
Ernest Korir <ernestbore3@gmail.com>
2023-07-14 20:23:15 +0300

    Create author.rb

--- app.rb
+++ app.rb
@@ -1,48 +1,93 @@
+require 'json'
+require_relative 'modules/books.module'
+require_relative 'modules/data.module'
+require_relative './modules/game_logic'
+require_relative './modules/author_logic'
+require_relative './classes/music_album_app'
+require_relative './classes/files_handler'
+require_relative './classes/genre_app'
 class App
-  attr_accessor :option
+  attr_accessor :option, :all_books, :all_labels, :all_authors, :all_genres
+
+  include BookModule
+  include LoadData
+  include DisplayItem
+
+  def initialize
+    @all_books = []
+    @all_labels = []
+    @all_genres = []
+    @all_authors = []
+    @all_genres = []
+    load_data
+  end
 
   def app_options
+    puts "Welcome to Catalog App!\n"
     puts 'Please choose an option by entering a number:'
     puts '1 - List all books'
     puts '2 - List all music albums'
-    puts '3 - List all movies'
-    puts '4 - List all games'
-    puts '5 - List all genres (e.g Comedy, Thriller)'
-    puts '6 - List all labels (e.g Gift, New)'
-    puts '7 - List all authors (e.g Stephen King)'
-    puts '8 - List all sources (e.g From a friend, Online shop)'
-    puts '9 - Add a book'
-    puts '10 - Add a music album'
-    puts '11 - Add a movie'
-    puts '12 - Add a game'
-    puts '13 - Exit'
+    puts '3 - List all games'
+    puts '4 - List all genres (e.g Comedy, Thriller)'
+    puts '5 - List all labels (e.g Gift, New)'
+    puts '6 - List all authors (e.g Stephen King)'
+    puts '7 - Add a book'
+    puts '8 - Add a music album'
+    puts '9 - Add a game'
+    puts '10 - Exit'
     print 'Your option '
     @option = gets.chomp
   end
 
   def check_option(option)
-    return unless (1..5).include?(option.to_i)
-
-    case option.to_i
-    when 1
-      list_all_books
-    end
-
-    def run
-      loop do
-        app_options
-
-        unless (1..13).include?(@option.to_i)
-          puts "Your input is invalid. Please enter then correct option value........\n"
-        end
-
-        if @option.to_i == 13
-          puts 'Thank you for using the app....'
-          break
-        end
-
-        check_option(@option)
+    if (1..5).include?(option.to_i)
+      case option.to_i
+      when 1
+        list_all_books
+      when 2
+        MusicAlbumApp.list_albums
+      when 3
+        GameLogic.list_games
+      when 4
+        GenreApp.list_genres
+      when 5
+        list_all_labels
       end
+    else
+
+      check_option_two(option.to_i)
+    end
+  end
+
+  def check_option_two(option)
+    case option
+    when 6
+      AuthorLogic.list_authors
+    when 7
+      book_info
+    when 8
+      MusicAlbumApp.add_album
+    when 9
+      GameLogic.add_game
+    end
+  end
+
+  def run
+    GenreApp.load_genres
+    MusicAlbumApp.load_albums
+    loop do
+      app_options
+
+      unless (1..10).include?(@option.to_i)
+        puts "Your input is invalid. Please enter then correct option value........\n"
+      end
+
+      if @option.to_i == 10
+        puts 'Thank you for using the app....'
+        break
+      end
+
+      check_option(@option)
     end
   end
 end
